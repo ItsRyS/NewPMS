4fe3445d02782d67ec3eba92dfd681d4017774f7
Patthanan Jaichuai <PatthananLT@gmail.com>
2025-01-11 20:14:00 +0700

    Revert "fix vite"
    This reverts commit e1d232ad7e93a92f61fd1754770439683b48a630.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  esbuild: {
-    loader: 'jsx', // กำหนดให้ esbuild ใช้ loader แบบ jsx
-  },
-  server: {
-    mimeTypes: {
-      'application/javascript': ['js', 'jsx'],
-    },
-  },
   plugins: [react()],
   build: {
-    sourcemap: false,
-  },
+    sourcemap: false
+  }
 });
