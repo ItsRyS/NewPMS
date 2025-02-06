e77c68dc287bc9af08e6a2dcd8a218d5eb314644
Patthanan Jaichuai <PatthananLT@gmail.com>
2025-02-06 13:36:52 +0700

    FIx heroku

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // สร้าง instance ของ Axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // ตั้งค่าฐาน URL สำหรับ API
+  baseURL: 'https://it-pms-eea67bd084d4.herokuapp.com/api', // ตั้งค่าฐาน URL สำหรับ API
   withCredentials: true, // เปิดใช้งาน cookie สำหรับการร้องขอ
 });
 
